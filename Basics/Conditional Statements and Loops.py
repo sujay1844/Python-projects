cfc418515632407123a852f179aa5b29bff92f4a
Sujay R <68642484+sujay1844@users.noreply.github.com>
2021-01-07 18:54:03 +0530

    Update Conditional Statements and Loops.py

--- Basics/Conditional Statements and Loops.py
+++ Basics/Conditional Statements and Loops.py
@@ -2,14 +2,18 @@
 # So I just learned the syntax of them in Python
 
 print("Hello! Welcome to Pasadena")
+
+# First, we create an array of names
 Array = ["Sheldon", "Leonard", "Penny", "Rajesh"]
+
+# Then, we check which name is being passed and print the corresponding statement
 for i in range(0,len(Array)):
     x = Array[i]
     if x == "Sheldon":
-        print("Hi Sheldon")
+        print("Hi geek")
     elif x == "Leonard":
-        print("Hi Leonard")
+        print("Hey shortie")
     elif x == "Penny":
-        print("Hi Penny")
+        print("Yo broke girl")
     else:
         print("I'm sorry. Who are you,", x,"\b?")
